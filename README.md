fc5fc8fb53f66c4784837fed107e3c4eda5e45a3
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-02-18 12:06:20 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# magic-numbers
+# README #
+
 A JavaScript module for working out the type of a file, based on its contents
---
+++ magic-numbers.js
@@ -0,0 +1,75 @@
+/*
+ * Module for finding the type of a file based on the first few bytes.
+ *
+ * By Will Voelcker
+ *
+ * http://www.willv.net
+ */
+
+
+var WV_MagicNumbers = (function() {
+
+	// Work out a file's mime-type from the start of its data.
+	// Patterns based on http://www.garykessler.net/library/file_sigs.html
+	// Mime types from PHP's finfo() function, and Google searches
+	function getMimeType(fileStartHex) {
+
+		// MPEG-4 Advanced Audio Coding (AAC) Low Complexity (LC) audio file
+		if (/^FFF[19]/.test(fileStartHex)) {
+			return "audio/aac";
+
+		// MPEG-1 Audio Layer 3 (MP3) audio file
+		} else if (/^494433/.test(fileStartHex)) {
+			return "audio/mpeg";
+
+		// MPEG audio file frame synch pattern
+		} else if (/^FFF[2-8A-Z]/.test(fileStartHex)) {
+			return "audio/mpeg";
+
+		// Apple Lossless Audio Codec file
+		// TODO:WV:20170520:I guess this might change to audio/mp4 one day - could adapt this code to try that if this mime-type causes an error...
+		} else if (/^.{8}667479704D344120/.test(fileStartHex)) {
+			return "audio/x-m4a";
+
+		// Wav file
+		// TODO:WV:20170520:Some browsers might require audio/wav or audio/wave.  Check this in all relevant ones.
+		} else if (/^52494646.{8}57415645666D7420/.test(fileStartHex)) {
+			return "audio/x-wav";
+
+		// Don't allow anything else
+		} else {
+			throw new Error("Unknown or invalid file type");
+		}
+	}
+
+	function base64toHEX(base64) {
+		var raw, HEX, _hex, i;
+
+		raw = atob(base64);
+		HEX = '';
+		for ( i = 0; i < raw.length; i++ ) {
+			_hex = raw.charCodeAt(i).toString(16)
+			HEX += (_hex.length==2?_hex:'0'+_hex);
+		}
+		return HEX.toUpperCase();
+	}
+
+	return {
+		"base64toHEX": base64toHEX,
+		"getMimeType": getMimeType
+	};
+
+}());
+
+// Export the object for use elsewhere
+if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+
+	// AMD. Register as an anonymous module.
+	define(function() {
+		return WV_MagicNumbers;
+	});
+} else if (typeof module !== 'undefined' && module.exports) {
+	module.exports = WV_MagicNumbers;
+} else if (typeof window != "undefined") {
+	window.WV_MagicNumbers = WV_MagicNumbers;
+}
